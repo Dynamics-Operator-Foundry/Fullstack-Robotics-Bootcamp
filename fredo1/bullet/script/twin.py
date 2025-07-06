97be498c0e0b94f7c4aa60974e7946091acc073f
pattylo <19451945as@gmail.com>
2025-07-06 20:08:17 +0800

    add fredo1 firmware

--- fredo1/bullet/script/twin.py
+++ fredo1/bullet/script/twin.py
@@ -3,52 +3,36 @@ import pybullet_data
 import time
 import math
 
-# Start physics simulation
-p.connect(p.GUI)
+status = p.connect(p.GUI)
+
 p.setGravity(0, 0, -9.81)
+
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setTimeStep(1.0 / 100.0)
 
 p.loadURDF("../urdf/plane.urdf")
 robot_id = p.loadURDF("../urdf/fredo1.urdf", useFixedBase=True)
 
-# Get joint info to check index
 num_joints = p.getNumJoints(robot_id)
 for i in range(num_joints):
     print(p.getJointInfo(robot_id, i))
 
-# Assuming joint_1 is index 0 — replace if needed
 joint_index = 2
 
-angle_range = range(0, 10000, 1)  # degrees
+angle_range = range(0, 10000, 1) 
 
 while True:
     for deg in list(angle_range):
-        # Your rotation logic here
-        # print(deg)
         
         deg = 0
         rad = 0
-        p.setJointMotorControl2(
-            bodyUniqueId=robot_id,
-            jointIndex=0,
-            controlMode=p.VELOCITY_CONTROL,
-            force=0.0
-        )
-        
-        p.setJointMotorControl2(
-            bodyUniqueId=robot_id,
-            jointIndex=1,
-            controlMode=p.VELOCITY_CONTROL,
-            force=0.0
-        )
-        
-        p.setJointMotorControl2(
-            bodyUniqueId=robot_id,
-            jointIndex=2,
-            controlMode=p.VELOCITY_CONTROL,
-            force=0.0
-        )
+        for joint_id in range(p.getNumJoints(robot_id)):
+            p.setJointMotorControl2(
+                robot_id, joint_id,
+                controlMode=p.VELOCITY_CONTROL,
+                force=0  # This disables motor control
+            )
         p.stepSimulation()
-        time.sleep(0.02)
+        time.sleep(0.01)
         print("hi")
 
